186e367ddead30db628817d7f44d29c52eb10a76
89VAnh <vietanha9a2@gmail.com>
2022-08-04 13:17:54 +0700

    ng-modul

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public name: string = 'VAnh';
+  public age: number = 18;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public resetName(): void {
+    this.name = '';
   }
-
 }
